51c6b5376f1c97f48c927a74f4dbe0272c04b521
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-08-12 20:54:08 +0200

    minor refactor

--- src/main/java/com/n1b3lung0/product/application/find/ProductResponse.java
+++ src/main/java/com/n1b3lung0/product/application/find/ProductResponse.java
@@ -2,5 +2,10 @@ package com.n1b3lung0.product.application.find;
 
 import java.math.BigDecimal;
 
-public record ProductResponse(String id, String name, String description, BigDecimal price) {
+public record ProductResponse(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
 }
